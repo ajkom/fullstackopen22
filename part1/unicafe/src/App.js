eadc9fbaafeb0cd9a1d1f14eba32e29d46b8c05a
Alex Komar <alexander.komar@fasolutions.com>
2022-04-25 13:47:41 +0300

    ex. 1.9

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,17 +5,18 @@ const Header = ({name}) => {
 }
 
 const Statictics = (props) => {
-  return (
-    <div>
-      <Header name="statistics" />
-
-      <p>good{props.good}</p>
-      <p>neutral {props.neutral}</p>
-      <p>bad {props.bad}</p>
-      <p>all {props.good + props.bad + props.neutral}</p>
-      <p>average {props.average()}</p>
-      <p>positive {props.positive()}%</p>
-    </div>)
+  let total = props.good + props.bad + props.neutral
+  if (total > 0)
+    return (
+      <div>
+        <p>good {props.good}</p>
+        <p>neutral {props.neutral}</p>
+        <p>bad {props.bad}</p>
+        <p>all {total}</p>
+        <p>average {props.average()}</p>
+        <p>positive {props.positive()}%</p>
+      </div>)
+  return <p>No feedback given</p>
 }
 
 const App = () => {
@@ -46,6 +47,8 @@ const App = () => {
       <button onClick={() => setGood(good+1)}>{"good"}</button>
       <button onClick={() => setNeutral(neutral+1)}>{"neutral"}</button>
       <button onClick={() => setBad(bad+1)}>{"bad"}</button>
+
+      <Header name="statistics" />
 
       <Statictics good={good} bad={bad} neutral={neutral}
       positive={() => positive()} average={() => average()}/>
